44680bfaa17ef01af9d5372ec8b0515644c87bcc
Colin Zeb <Colin@mmin.us>
2017-05-03 09:01:20 +0800

    添加nuget包信息

--- System/Properties/AssemblyInfo.cs
+++ System/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("System")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("System扩展")]
+[assembly: AssemblyDescription("针对Linq，String等的一些扩展")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("System")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("JingShOnline")]
+[assembly: AssemblyProduct("System.Extend")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.5.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
