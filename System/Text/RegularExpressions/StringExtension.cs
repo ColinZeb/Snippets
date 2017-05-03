c3a10d0ae360945499834ca8f84178f08cb8000a
Colin Zeb <Colin@mmin.us>
2017-05-03 13:24:24 +0800

    加入regex

---
+++ System/Text/RegularExpressions/StringExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace System.Text.RegularExpressions
+{
+    public static class StringExtension
+    {
+        /// <summary>
+        /// 是否匹配指定的正则表达式
+        /// </summary>
+        /// <param name="text">要匹配的文本</param>
+        /// <param name="pattern">要匹配的正则表达式</param>
+        /// <returns></returns>
+        public static bool IsMatch(this string text, string pattern)
+        {
+            return Regex.IsMatch(text, pattern: pattern);
+        }
+    }
+}
