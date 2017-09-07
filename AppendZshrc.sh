17abc378aa3b51908346fb11f7172e7982c266f3
ColinZeb <colin@mmin.us>
2017-09-07 09:35:45 +0800

    add bindkey

--- AppendZshrc.sh
+++ AppendZshrc.sh
@@ -1,4 +1,6 @@
 if [ -f $ZSH_CUSTOM/plugins/Snippets/.alias ]; then
         . $ZSH_CUSTOM/plugins/Snippets/.alias
 fi
-
+if [ -f $ZSH_CUSTOM/plugins/Snippets/.bindzshkeyrc ]; then
+	source $ZSH_CUSTOM/plugins/Snippets/.bindzshkeyrc
+fi
