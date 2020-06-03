695acca6992356280f7031aba6d145663a869105
colin <colin@mmin.us>
2020-06-03 20:21:39 +0800

    nginx branch

--- AppendZshrc.sh
+++
@@ -1,7 +0,0 @@
-if [ -f $ZSH_CUSTOM/plugins/Snippets/.alias ]; then
-        . $ZSH_CUSTOM/plugins/Snippets/.alias
-fi
-if [ -f $ZSH_CUSTOM/plugins/Snippets/.bindzshkeyrc ]; then
-	source $ZSH_CUSTOM/plugins/Snippets/.bindzshkeyrc
-fi
-setopt no_nomatch
